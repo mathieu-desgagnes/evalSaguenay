bc2c6dc45e97f107e01d3c6e9223e3e3330c3d6f
Mathieu Desgagnés <mathieu.desgagnes@dfo-mpo.gc.ca>
2026-06-01 14:25:21 -0400

    ajouter les données de 2021 à ajourd'hui pour le sdonnées biologiques

---
+++ R/ajout_donnees_bio.R
@@ -0,0 +1,80 @@
+#' Ajouter les données récentes aux données historiques de données biologiques.
+#'
+#' Les nouvelles données sont présentent dans le dossier "dirInput2021etPlus", chaque nouvelle années dans un dossier avec comme nom "Saison AAAA"
+#'
+#' @param donnees_2020_et_moins `data.frame` contenant les données historique consolidées
+#' @param input_2021_et_plus_dir chemin du dossier contenant des dossier "Saison 20xx" avec les nouvelles données
+#'
+#' @importFrom readxl read_excel
+#'
+#' @return le data.frame() consolidé
+#'
+ajout_donnees_bio <- function(
+  donnees_2020_et_moins = NULL,
+  input_2021_et_plus_dir
+) {
+  db.init <- db.2020etMoins
+  temp <- dir(dirInput2021etPlus)
+  annee <- substr(temp[which(substr(temp, 1, 7) == 'Saison ')], 8, 11)
+  db.temp <- as.data.frame(array(
+    NA,
+    dim = c(0, 12),
+    dimnames = list(
+      NULL,
+      c(
+        'partenaire',
+        'nomSite',
+        'echantillonneur',
+        'annee',
+        'mois',
+        'jour',
+        'espece',
+        'longueur',
+        'poids',
+        'commentaire',
+        'anneeGestion',
+        'date'
+      )
+    )
+  ))
+  for (i.an in annee) {
+    print(i.an)
+    db.new <- as.data.frame(readxl::read_excel(
+      path = file.path(
+        dirInput2021etPlus,
+        paste0('Saison ', i.an),
+        paste0('donneeBiologique_', i.an, '.xlsx')
+      ),
+      sheet = 1,
+      na = 'NA'
+    ))
+    names(db.new) <- c(
+      'partenaire',
+      'nomSite',
+      'echantillonneur',
+      'annee',
+      'mois',
+      'jour',
+      'espece',
+      'longueur',
+      'poids',
+      'commentaire'
+    )
+    if (nrow(db.new) > 0) {
+      db.new$anneeGestion <- as.numeric(i.an)
+      db.new$date <- as.POSIXct(
+        paste(db.new$annee, db.new$mois, db.new$jour, sep = '-'),
+        format = '%Y-%m-%d'
+      )
+      db.new$nomSite <- standardiser_nom_site(sites = db.new$nomSite)[, 'sites']
+      db.new$espece <- standardiser_nom_espece(espece = db.new$espece)
+      db.temp <- merge(db.temp, db.new, all = TRUE)
+    }
+  }
+  ##
+  ## nrow(db.init); nrow(db.temp)
+  db <- merge(db.init, db.temp, all = TRUE)
+  ## nrow(db)
+  ##
+  return(db)
+}
--- R/lire_donnees.R
+++ R/lire_donnees.R
@@ -19,7 +19,40 @@ lire_donnees <- function(
   } else {
     stop('Vérifier la disponibilité des données pré-2021.')
   }
+
   ##
   ##
-  ## ajouter les données post-2021
+  ## ajouter les données de 2021 et plus
+  db <- ajout_donnees_bio(
+    donnees_2020_et_moins = data$db,
+    input_2021_et_plus_dir = dir_input
+  )
+  ## sauvegarder la base de donnée consolidée
+  save(db, file = file.path(dir_input, 'donnees_DB.RData'))
+  write.csv2(db, file = file.path(dir_input, 'donnees_DB.csv'))
+
+  jb <- ajoutJournauxBords(
+    fichier2020etMoins = file.path(
+      dirSag,
+      'Données',
+      'journauxBords_2015-2020.RData'
+    ),
+    dirInput2021etPlus = file.path(dirSag, 'Données')
+  )
+  ## sauvegarder la base de donnée consolidée
+  save(jb, file = file.path(dirSag, 'Données', 'donnees_JB.RData'))
+  write.csv2(jb, file = file.path(dirSag, 'Données', 'donnees_JB.csv'))
+
+  ech <- ajoutEchantillonneurs(
+    fichier2020etMoins = file.path(
+      dirSag,
+      'Données',
+      'echantillonneurs_1995-2020.RData'
+    ),
+    dirInput2021etPlus = file.path(dirSag, 'Données')
+  )
+  ## sauvegarder la base de donnée consolidée
+  save(ech, file = file.path(dirSag, 'Données', 'donnees_ech.RData'))
+  write.csv2(ech, file = file.path(dirSag, 'Données', 'donnees_ech.csv'))
+  ## load(file.path(dirSag,'Données','donnees_ech.RData'), verbose=1)
 }
