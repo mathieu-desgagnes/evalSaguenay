d9cd01f1fb1595d0cdd33c56c510761784308da7
Mathieu Desgagnés <mathieu.desgagnes@dfo-mpo.gc.ca>
2026-06-01 11:06:22 -0400

    Transition vers élimination du ficher "lire-donness.r"

--- R/lire_donnees.R
+++ R/lire_donnees.R
@@ -1,6 +1,6 @@
-lire_donnees <- function() {
-  dir_input <- file.path('S:', 'Saguenay', 'Données')
-  dir_output <- file.path(
+lire_donnees <- function(
+  dir_input = file.path('S:', 'Saguenay', 'Données'),
+  dir_output = file.path(
     'C:',
     'Users',
     'DesgagnesMa',
@@ -8,23 +8,11 @@ lire_donnees <- function() {
     'evalSaguenay2026',
     'output'
   )
-  if (!dir.exists(file.path(dir_output))) {
-    dir.create(file.path(dir.output))
-  } # créer le dossier s'il n'existe pas
-  if (!dir.exists(file.path(dir_output, 'fr'))) {
-    dir.create(file.path(dir_output, 'fr'))
-  } # créer le dossier s'il n'existe pas
-  if (!dir.exists(file.path(dir_output, 'en'))) {
-    dir.create(file.path(dir_output, 'en'))
-  } # créer le dossier s'il n'existe pas
-  if (!dir.exists(file.path(dir_output, 'bil'))) {
-    dir.create(file.path(dir_output, 'bil'))
-  } # créer le dossier s'il n'existe pas
-  if (!dir.exists(file.path(dir_output, 'csv'))) {
-    dir.create(file.path(dir_output, 'csv'))
-  } # créer le dossier s'il n'existe pas
-
-  load(file = file.path(dir.input, 'donnees_ech.RData'), verbose = TRUE) #pour reprendre une session précédente sans refaire tous les calculs
+) {
+  ## vérifier si les données biologiques pré-2021 existent, les recalculer sinon
+  if (NULL) {
+    load(file = file.path(dir.input, 'donnees_ech.RData'), verbose = TRUE)
+  } #pour reprendre une session précédente sans refaire tous les calculs
   file.info(file.path(dir.input, 'donnees_ech.RData'))$mtime
   ## ech.init <- read.table(file=file.path(dir.input, 'donnees_ech.csv'), sep=';',dec=, na='', header=T, as.is=TRUE)
   ech.init <- ech
