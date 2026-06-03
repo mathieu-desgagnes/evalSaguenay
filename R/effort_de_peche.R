6bc1558c0934e25c0fb5cd417ac22d048a71374c
Mathieu Desgagnés <mathieu.desgagnes@dfo-mpo.gc.ca>
2026-06-03 11:41:56 -0400

    initial de la fonction pour produire graphique d'effort de pêche

---
+++ R/effort_de_peche.R
@@ -0,0 +1,419 @@
+#' Calcul l'effort de pêche sur le Saguenay et produit les graphiques correspondants
+#'
+#' @param ech données récolétées dans le volet 1 du projet (échantillonneurs)
+#'
+#' @returns
+#' @export
+#'
+#' @examples
+effort_de_peche <- function(ech) {
+  ech.init$anneeGestion <- as.numeric(ech.init$anneeGestion)
+  ech.init <- ech.init[ech.init$anneeGestion %in% annees, ]
+
+  ## préparation des données
+  ##
+  ## echantillons
+  ech.init$nbHeures <- as.numeric(ech.init$nbHeures)
+  ech.init$nbHeures[which(ech.init$nbHeures < 0.25)] <- 0.25 #si le nombre d'heure est inférieur à 0.25 alors on met 0.25
+  ech.init$nbGadide <- ech.init$nbMorue + ech.init$nbOgac
+  ech.init[ech.init$anneeGestion < 2001, c('nbMorue', 'nbOgac')] <- NA
+  ech.init$ue <- ech.init$nbLignes * ech.init$nbHamecons * ech.init$nbHeures #unite d'effort
+  ech.init$logUE <- log(ech.init$ue)
+  ech.init$sfs.ori <- ech.init$sfs
+  ech.init$sfs <- as.numeric(wday(ech.init$date) %in% c(1, 7)) #semaine=0, fds=1
+  ## as.numeric(wday(as.POSIXct('2023-11-01')) %in% c(1,7))
+  ## as.numeric(wday(as.POSIXct('2023-10-29')) %in% c(1,7))
+  ## which(ech.init$sfs.ori != ech.init$sfs)
+  if (FALSE) {
+    #exploration du nombre d'heures de pêche
+    nrow(ech.init[which(ech.init$secteur == "fond"), ])
+    nrow(ech.init[
+      which(ech.init$secteur == "fond" & ech.init$nbHeures <= 12),
+    ])
+    nrow(ech.init[which(ech.init$secteur == "fond" & ech.init$nbHeures < 12), ])
+    nrow(ech.init[which(ech.init$secteur == "fond" & ech.init$nbHeures <= 8), ])
+    nrow(ech.init[which(ech.init$secteur == "fond" & ech.init$nbHeures < 8), ])
+    nrow(ech.init[which(ech.init$secteur == "fond" & ech.init$nbHeures <= 6), ])
+    nrow(ech.init[which(ech.init$secteur == "fond" & ech.init$nbHeures < 6), ])
+    ##
+    hist(
+      ech.init[, 'nbHeures'],
+      breaks = seq(-0.25, 200, by = 1),
+      xlim = c(0, 30),
+      ylim = c(0, 0.2),
+      main = 'Nombre heures de pêche',
+      freq = FALSE
+    )
+    x <- hist(
+      ech.init[ech.init$annee %in% c(1994:1998), 'nbHeures'],
+      breaks = seq(-0.25, 200, by = 1),
+      plot = FALSE
+    )
+    lines(x$mids, x$density, col = 1)
+    x <- hist(
+      ech.init[ech.init$annee %in% c(1999:2003), 'nbHeures'],
+      breaks = seq(-0.25, 200, by = 1),
+      plot = FALSE
+    )
+    lines(x$mids, x$density, col = 2)
+    x <- hist(
+      ech.init[ech.init$annee %in% c(2004:2008), 'nbHeures'],
+      breaks = seq(-0.25, 200, by = 1),
+      plot = FALSE
+    )
+    lines(x$mids, x$density, col = 3)
+    x <- hist(
+      ech.init[ech.init$annee %in% c(2009:2013), 'nbHeures'],
+      breaks = seq(-0.25, 200, by = 1),
+      plot = FALSE
+    )
+    lines(x$mids, x$density, col = 4)
+    x <- hist(
+      ech.init[ech.init$annee %in% c(2014:2018), 'nbHeures'],
+      breaks = seq(-0.25, 200, by = 1),
+      plot = FALSE
+    )
+    lines(x$mids, x$density, col = 5)
+    x <- hist(
+      ech.init[ech.init$annee %in% c(2019:2023), 'nbHeures'],
+      breaks = seq(-0.25, 200, by = 1),
+      plot = FALSE
+    )
+    lines(x$mids, x$density, col = 6)
+  }
+  ech <- ech.init[which(ech.init$secteur == "fond"), ]
+
+  ## visites
+  ## lorsqu'il y a plusieurs échantillons (visites) d'un même site une même journée, faire la moyenne des nombres de pêcheurs
+  ## modif: ne pas tenir compte du nombre de pêcheurs Pelag -> retire 7 cas seulement
+  a.enlever <- vector() #lignes de "fausses" visite à retirer de la liste après traitement
+  visites.ori <- ech[
+    !duplicated(ech[, c(
+      "anneeGestion",
+      "date",
+      "nomSite",
+      "sfs",
+      "nbPecheursFond",
+      "echantillonneur"
+    )]),
+  ]
+  visites.temp <- visites.ori[
+    order(visites.ori[, 'date'], visites.ori[, 'nomSite']),
+  ]
+  visites.temp$rowNb <- 1:nrow(visites.temp)
+  for (i.vis in 1:(length(visites.temp$rowNb) - 1)) {
+    j <- i.vis + 1
+    if (
+      visites.temp$date[i.vis] == visites.temp$date[i.vis + 1] &
+        visites.temp$nomSite[i.vis] == visites.temp$nomSite[i.vis + 1]
+    ) {
+      while (
+        j != nrow(visites.temp) &
+          visites.temp$date[j] == visites.temp$date[j + 1] &
+          visites.temp$nomSite[j] == visites.temp$nomSite[j + 1]
+      ) {
+        j <- j + 1
+      }
+      if (!is.na(mean(visites.temp$nbPecheursFond[i.vis:j], na.rm = TRUE))) {
+        visites.temp$nbPecheursFond[i.vis] <- mean(
+          visites.temp$nbPecheursFond[i.vis:j],
+          na.rm = TRUE
+        )
+      }
+      if (!is.na(mean(visites.temp$nbPecheursPelag[i.vis:j], na.rm = TRUE))) {
+        visites.temp$nbPecheursPelag[i.vis] <- mean(
+          visites.temp$nbPecheursPelag[i.vis:j],
+          na.rm = TRUE
+        )
+      }
+      a.enlever <- c(a.enlever, (i.vis + 1):j)
+    }
+  }
+  visites.init <- visites.temp[
+    -a.enlever,
+    c(
+      'anneeGestion',
+      'date',
+      'nomSite',
+      'sfs',
+      'secteur',
+      'nbPecheursFond',
+      'echantillonneur',
+      'annee',
+      'engin',
+      'affiliationEchantillonneur',
+      'mois',
+      'jour',
+      'jourSemaine',
+      'lundiAuVendredi',
+      'nbPecheursPelag',
+      'noSite',
+      'especeVisee',
+      'nbPecheursTot',
+      'nbCabanesOccFond',
+      'nbPecheursFondSScabane',
+      'nbCabanesOccPelag',
+      'nbPecheursPelagSScabane',
+      'nValideNbPecheursPelag',
+      'nValideNbPecheursFond',
+      'nValideNbTotPecheurs',
+      'nbPecheursSecteur',
+      'nbPecheursMorue',
+      'nbPecheursSebastes',
+      'pecheurDedie',
+      'nomSite.ori'
+    )
+  ]
+  ##
+  ## nrow(visites.temp); nrow(visites.init)
+  ## étude des données de visites
+  if (FALSE) {
+    ## à plusieurs endroit, il semble y avoir "visite multiple" simplement parce que la deuxième ligne est "NA" pour le nombre de pêcheurs
+    ## l'approche de faire la moyenne gere ces cas sans difficulté
+    visites.ori <- ech[
+      !duplicated(ech[, c(
+        "anneeGestion",
+        "date",
+        "nomSite",
+        "sfs",
+        "nbPecheursFond",
+        "echantillonneur"
+      )]),
+    ]
+    visites.temp <- visites.ori[
+      order(visites.ori[, 'date'], visites.ori[, 'nomSite']),
+    ]
+    visites.temp$rowNb <- 1:nrow(visites.temp)
+    temp <- NULL
+    for (i.vis in 1:(length(visites.temp$rowNb) - 1)) {
+      j <- i.vis + 1
+      if (
+        visites.temp$date[i.vis] == visites.temp$date[i.vis + 1] &
+          visites.temp$nomSite[i.vis] == visites.temp$nomSite[i.vis + 1]
+      ) {
+        while (
+          j != nrow(visites.temp) &
+            visites.temp$date[j] == visites.temp$date[j + 1] &
+            visites.temp$nomSite[j] == visites.temp$nomSite[j + 1]
+        ) {
+          j <- j + 1
+        }
+        temp <- rbind(temp, visites.temp[i.vis:j, ])
+      }
+    }
+    write.csv2(temp, file = file.path(dir.output, 'exploDonneesVisite.csv'))
+  }
+
+  aggregate(
+    visites.init$nbPecheursFond,
+    visites.init['anneeGestion'],
+    FUN = sum,
+    na.rm = TRUE
+  )
+
+  ## nombre de visites par année
+  plot(aggregate(
+    visites.init$anneeGestion,
+    visites.init['anneeGestion'],
+    FUN = length
+  ))
+  ## nombre de pêcheurs interrogés par année
+  plot(aggregate(ech$anneeGestion, ech['anneeGestion'], FUN = length))
+  abline(
+    h = mean(aggregate(ech$anneeGestion, ech['anneeGestion'], FUN = length)$x)
+  )
+  mean(tail(
+    aggregate(ech$anneeGestion, ech['anneeGestion'], FUN = length)$x,
+    15
+  ))
+  ## nombre de poisson mesurés
+  plot(aggregate(db$anneeGestion, db['anneeGestion'], FUN = length))
+  temp <- aggregate(db$anneeGestion, db['anneeGestion'], FUN = length)
+  mean(tail(temp$x, 10))
+  ## nombre de journaux de bord
+  plot(aggregate(jb$anneeGestion, jb['anneeGestion'], FUN = length))
+  mean(aggregate(jb$anneeGestion, jb['anneeGestion'], FUN = length)$x)
+  mean(table(
+    aggregate(
+      jb$anneeGestion,
+      jb[, c('echantillonneur', 'anneeGestion')],
+      FUN = length
+    )$anneeGestion
+  ))
+
+  ## nbespeces autres
+  ## dimnames(ech)[[2]][which(substring(dimnames(ech)[[2]], 1, 2)=='nb')]
+  ech$nbAutre <- as.numeric(ech$nbAutre)
+  ech$nbToutAutre <- apply(
+    ech[, c(
+      "nbFletan",
+      "nbLycode",
+      "nbSaida",
+      "nbRaie",
+      "nbMerluche",
+      "nbAutre",
+      "nbAutre2",
+      "nbLimace",
+      "nbPlie",
+      "nbGoberge",
+      "nbHareng",
+      "nbMerlucheEcureuil"
+    )],
+    1,
+    sum,
+    na.rm = TRUE
+  )
+  ## aggregate(ech$toutAutre, ech['anneeGestion'], FUN=sum, na.rm=TRUE)
+
+  ##
+  ## clacul de nombre de jour de pêche par site par année
+  ## déterminer le nombre de jours entre premier et dernier échantillon d'une année (de fin de semaine et de semaine)
+  sfs.nbJour.an <- array(
+    dim = c(length(annees), 2),
+    dimnames = list(annee = annees, c('semaine', 'fds'))
+  )
+  for (i.an in seq_along(annees)) {
+    cetteAnnee <- visites.init[
+      which(visites.init$anneeGestion == annees[i.an]),
+    ]
+    if (nrow(cetteAnnee) > 0) {
+      temp <- wday(seq(
+        min(cetteAnnee$date, na.rm = TRUE),
+        max(cetteAnnee$date, na.rm = TRUE),
+        by = 'day'
+      ))
+      temp2 <- month(seq(
+        min(cetteAnnee$date, na.rm = TRUE),
+        max(cetteAnnee$date, na.rm = TRUE),
+        by = 'day'
+      ))
+      if (annees[i.an] %in% c(2015:2016)) {
+        #semaine, mesure de gestion interdisant la pêche les mardi durant ces années
+        sfs.nbJour.an[i.an, 'semaine'] <- length(which(
+          temp %in% c(1, 3:5) & temp2 %in% c(1:2)
+        )) +
+          length(which(temp %in% c(1:5) & temp2 %in% c(3)))
+      } else {
+        sfs.nbJour.an[i.an, 'semaine'] <- length(which(temp %in% 1:5))
+      }
+      sfs.nbJour.an[i.an, 'fds'] <- length(which(temp %in% c(6:7))) #fin de semaine
+    } else {
+      sfs.nbJour.an[i.an, ] <- c(0, 0)
+    }
+  }
+
+  ##
+  ## effort de pêche par site
+  ##
+  nomPng <- 'effortPeche.site'
+  for (i.langue in c('fr', 'en', 'bil')) {
+    png(
+      file = file.path(dir.output, i.langue, paste0(nomPng, '.png')),
+      height = 8,
+      width = 9,
+      units = 'in',
+      res = 300
+    )
+    temp <- effortPeche.site(
+      ech = subset(ech.init, nbHeures < 12),
+      visites = visites.init,
+      langue = i.langue
+    )
+    dev.off()
+  }
+  nbPecheursMoy.site.sfs <- temp
+  nbPecheursTot.site <- array(
+    dim = dim(nbPecheursMoy.site.sfs)[1:2],
+    dimnames = dimnames(nbPecheursMoy.site.sfs)[1:2]
+  )
+  for (i.an in seq_along(annees)) {
+    for (i.site in seq_along(sites)) {
+      nbPecheursTot.site[sites[i.site], as.character(annees[i.an])] <-
+        sum(
+          nbPecheursMoy.site.sfs[
+            sites[i.site],
+            as.character(annees[i.an]),
+            c('semaine', 'fds'),
+            'moyenne'
+          ] *
+            sfs.nbJour.an[as.character(annees[i.an]), c('semaine', 'fds')]
+        )
+    }
+  }
+  write.csv2(
+    nbPecheursTot.site,
+    file = file.path(dir.output, 'csv', paste0(nomPng, '.csv'))
+  )
+  save(
+    nbPecheursTot.site,
+    file = file.path(dir.output, 'csv', 'nbPecheursTot_site.RData')
+  )
+  write.csv2(
+    nbPecheursMoy.site.sfs,
+    file = file.path(dir.output, 'csv', 'nbPecheursMoy_strate.csv')
+  )
+  save(
+    nbPecheursMoy.site.sfs,
+    file = file.path(dir.output, 'csv', 'nbPecheursMoy_strate.RData')
+  )
+
+  load(
+    file = file.path(dir.output, 'csv', 'nbPecheursTot_site.RData'),
+    verbose = 1
+  )
+  load(
+    file = file.path(dir.output, 'csv', 'nbPecheursMoy_strate.RData'),
+    verbose = 1
+  )
+
+  ##
+  ## effort de pêche total
+  ##
+  nomPng <- 'effortPeche'
+  for (i.langue in c('fr', 'en', 'bil')) {
+    png(
+      file = file.path(dir.output, i.langue, paste0(nomPng, '.png')),
+      height = 5,
+      width = 8,
+      units = 'in',
+      res = 300
+    )
+    temp <- effortPeche(
+      nbPecheurs = nbPecheursMoy.site.sfs,
+      nbJours = sfs.nbJour.an,
+      langue = i.langue
+    )
+    dev.off()
+  }
+  nbPecheursTot.secteur <- temp
+  write.csv2(temp, file = file.path(dir.output, 'csv', paste0(nomPng, '.csv')))
+  save(
+    nbPecheursTot.secteur,
+    file = file.path(dir.output, 'csv', paste0(nomPng, '.RData'))
+  )
+
+  load(
+    file = file.path(dir.output, 'csv', paste0(nomPng, '.RData')),
+    verbose = 1
+  )
+
+  ##
+  ## effort de pêche total, en barplot, 1x2
+  ##
+  nomPng <- 'effortPeche.barplot'
+  for (i.langue in c('fr', 'en', 'bil')) {
+    png(
+      file = file.path(dir.output, i.langue, paste0(nomPng, '.png')),
+      height = 4.5,
+      width = 9,
+      units = 'in',
+      res = 300
+    )
+    temp <- effortPeche.barplot(
+      nbPecheursTot = nbPecheursTot.secteur,
+      langue = i.langue
+    )
+    dev.off()
+  }
+  write.csv2(temp, file = file.path(dir.output, 'csv', paste0(nomPng, '.csv')))
+}
