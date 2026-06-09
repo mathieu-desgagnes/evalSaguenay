81cd692ca3c56cdff682ed15e09254b6ef255208
Mathieu Desgagnés <mathieu.desgagnes@dfo-mpo.gc.ca>
2026-06-09 14:55:20 -0400

    ajustement des noms de variable des échantillonneurs

--- R/ajout_echantillonneurs.R
+++ R/ajout_echantillonneurs.R
@@ -23,105 +23,81 @@ ajout_echantillonneurs <- function(
       na = 'NA'
     ))
   }
-  names(ech.new) <- c(
-    'affiliationEchantillonneur',
-    'echantillonneur',
-    'annee',
-    'mois',
-    'jour',
-    'heure',
-    'sfs',
-    'nomSite',
-    'nbPecheursFond',
-    'noPecheur',
-    'secteur',
-    'engin',
-    'nbLignes',
-    'nbHamecons',
-    'nbHeures',
-    'echosondeur',
-    'prof',
-    'nbMorue',
-    'nbOgac',
-    'nbSebastes',
-    'nbTurbot',
-    'nbCrabe',
-    'nbLycode',
-    'nbRaie',
-    'nbMerluche',
-    'nbEperlan',
-    'nbFletan',
-    'nbSaida',
-    'commentaires'
+
+  # names(ech.new) <- c(
+  #   'affiliationEchantillonneur',
+  #   'echantillonneur',
+  #   'annee',
+  #   'mois',
+  #   'jour',
+  #   'heure',
+  #   'sfs',
+  #   'site',
+  #   'nbPecheursFond',
+  #   'noPecheur',
+  #   'secteur',
+  #   'engin',
+  #   'nbLignes',
+  #   'nbHamecons',
+  #   'nb_heure',
+  #   'sonar',
+  #   'prof',
+  #   'nbMorue',
+  #   'nbOgac',
+  #   'nbSebastes',
+  #   'nbTurbot',
+  #   'nbCrabe',
+  #   'nbLycode',
+  #   'nbRaie',
+  #   'nbMerluche',
+  #   'nbEperlan',
+  #   'nbFletan',
+  #   'nbSaida',
+  #   'commentaires'
+  # )
+  ech.new$date <- as.POSIXct(
+    paste(ech.new$annee, ech.new$mois, ech.new$jour, sep = '-'),
+    format = '%Y-%m-%d'
   )
-  if (nrow(ech.new) > 0) {
-    ech.new$date <- as.POSIXct(
-      paste(ech.new$annee, ech.new$mois, ech.new$jour, sep = '-'),
-      format = '%Y-%m-%d'
-    )
-    ech.new$nomSite <- standardiser_nom_site(site = ech.new$nomSite)[,
-      'sites'
-    ]
-    ech.new$annee <- lubridate::year(ech.new$date)
-    ech.new$mois <- lubridate::month(ech.new$date)
-    ech.new$jour <- lubridate::day(ech.new$date)
-    ech.new$jourSemaine <- lubridate::wday(ech.new$date) #1=dimanche
-    ech.new$lundiAuVendredi <- ech.new$jourSemaine %in% 2:6
-    ##
-    ## Nettoyer les données
-    ## les nombres == NA transformés en 0
-    temp <- names(ech.new)[which(substr(names(ech.new), 1, 2) == 'nb')]
-    especes <- temp[which(
-      !temp %in%
-        c(
-          "nbPecheursPelag",
-          "nbPecheursFond",
-          "nbLignes",
-          "nbHamecons",
-          "nbHeures",
-          "nbPecheursTot",
-          "nbCabanesOccFond",
-          "nbPecheursFondSScabane",
-          "nbCabanesOccPelag",
-          "nbPecheursPelagSScabane",
-          "nbPecheursSecteur",
-          "nbPecheursMorue",
-          "nbPecheursSebastes"
-        )
-    )]
-    for (i.esp in especes) {
-      ech.new[is.na(ech.new[i.esp]), i.esp] <- 0
-    }
-    ech.new$anneeGestion <- ech.new$annee
-    ##
-    ech.init <- merge(ech.init, ech.new, all = TRUE)
-  }
+  ech.new$site <- standardiser_nom_site(site = ech.new$site)[,
+    'sites'
+  ]
+  ech.new$jourSemaine <- lubridate::wday(ech.new$date) #1=dimanche
+  ech.new$lundiAuVendredi <- ech.new$jourSemaine %in% 2:6
+  ##
+  ech.new$anneeGestion <- ech.new$annee
+  ##
+
+  ech.init <- merge(ech.init, ech.new, all = TRUE)
+  ## nrow(ech.init); nrow(ech.new); nrow(ech)
+
   ##
   ## nrow(ech.init)
   ech.init$engin <- standardiser_nom_engin(ech.init$engin)
-  ## table(ech.init$echosondeur)
+  ## table(ech.init$sonar)
   ech.init[
-    ech.init$echosondeur %in% c('O', 'o', 'oui', 'OUI'),
-    'echosondeur'
-  ] <- TRUE
+    ech.init$sonar %in% c('O', 'o', 'oui', 'OUI', TRUE),
+    'sonar'
+  ] <- 1
   ech.init[
-    ech.init$echosondeur %in% c('N', 'n', 'non', 'NON'),
-    'echosondeur'
-  ] <- FALSE
+    ech.init$sonar %in% c('N', 'n', 'non', 'NON', FALSE),
+    'sonar'
+  ] <- 0
   ech.init[
-    ech.init$echosondeur %in% c('nd', 'O/N', 'P', '?', '17', '2', '3'),
-    'echosondeur'
+    ech.init$sonar %in% c('nd', 'O/N', 'P', '?', '17', '2', '3'),
+    'sonar'
   ] <- NA
   ##
   ##
   ech.init$anneeGestion <- as.numeric(ech.init$anneeGestion)
   # plot(ech.init$annee, ech.init$anneeGestion)
-  ech.init$nbHeures <- as.numeric(ech.init$nbHeures)
-  ech.init$nbHeures[which(ech.init$nbHeures < 0.25)] <- 0.25 #si le nombre d'heure est inférieur à 0.25 alors on met 0.25
-  ech.init$nbGadide <- ech.init$nbMorue + ech.init$nbOgac
-  ech.init[ech.init$anneeGestion < 2001, c('nbMorue', 'nbOgac')] <- NA
-  ech.init$ue <- ech.init$nbLignes * ech.init$nbHamecons * ech.init$nbHeures #unite d'effort
+  ech.init$nb_heure <- as.numeric(ech.init$nb_heure)
+  ech.init$nb_heure[which(ech.init$nb_heure < 0.25)] <- 0.25 #si le nombre d'heure est inférieur à 0.25 alors on met 0.25
+  ech.init$gadide <- ech.init$morue + ech.init$ogac
+  ech.init[which(ech.init$anneeGestion < 2001), c('morue', 'ogac')] <- NA
+  ech.init$ue <- ech.init$nb_ligne * ech.init$nb_hamecon * ech.init$nb_heure #unite d'effort
   ech.init$logUE <- log(ech.init$ue)
+  ech.init$sfs <- ech.init[, 'semaine(0)_FinDeSemaine(1)']
   ech.init$sfs.ori <- ech.init$sfs
   ech.init$sfs <- as.numeric(wday(ech.init$date) %in% c(1, 7)) #semaine=0, fds=1
   ## as.numeric(wday(as.POSIXct('2023-11-01')) %in% c(1,7))
@@ -131,16 +107,16 @@ ajout_echantillonneurs <- function(
     #exploration du nombre d'heures de pêche
     nrow(ech.init[which(ech.init$secteur == "fond"), ])
     nrow(ech.init[
-      which(ech.init$secteur == "fond" & ech.init$nbHeures <= 12),
+      which(ech.init$secteur == "fond" & ech.init$nb_heure <= 12),
     ])
-    nrow(ech.init[which(ech.init$secteur == "fond" & ech.init$nbHeures < 12), ])
-    nrow(ech.init[which(ech.init$secteur == "fond" & ech.init$nbHeures <= 8), ])
-    nrow(ech.init[which(ech.init$secteur == "fond" & ech.init$nbHeures < 8), ])
-    nrow(ech.init[which(ech.init$secteur == "fond" & ech.init$nbHeures <= 6), ])
-    nrow(ech.init[which(ech.init$secteur == "fond" & ech.init$nbHeures < 6), ])
+    nrow(ech.init[which(ech.init$secteur == "fond" & ech.init$nb_heure < 12), ])
+    nrow(ech.init[which(ech.init$secteur == "fond" & ech.init$nb_heure <= 8), ])
+    nrow(ech.init[which(ech.init$secteur == "fond" & ech.init$nb_heure < 8), ])
+    nrow(ech.init[which(ech.init$secteur == "fond" & ech.init$nb_heure <= 6), ])
+    nrow(ech.init[which(ech.init$secteur == "fond" & ech.init$nb_heure < 6), ])
     ##
     hist(
-      ech.init[, 'nbHeures'],
+      ech.init[, 'nb_heure'],
       breaks = seq(-0.25, 200, by = 1),
       xlim = c(0, 30),
       ylim = c(0, 0.2),
@@ -148,37 +124,37 @@ ajout_echantillonneurs <- function(
       freq = FALSE
     )
     x <- hist(
-      ech.init[ech.init$annee %in% c(1994:1998), 'nbHeures'],
+      ech.init[ech.init$annee %in% c(1994:1998), 'nb_heure'],
       breaks = seq(-0.25, 200, by = 1),
       plot = FALSE
     )
     lines(x$mids, x$density, col = 1)
     x <- hist(
-      ech.init[ech.init$annee %in% c(1999:2003), 'nbHeures'],
+      ech.init[ech.init$annee %in% c(1999:2003), 'nb_heure'],
       breaks = seq(-0.25, 200, by = 1),
       plot = FALSE
     )
     lines(x$mids, x$density, col = 2)
     x <- hist(
-      ech.init[ech.init$annee %in% c(2004:2008), 'nbHeures'],
+      ech.init[ech.init$annee %in% c(2004:2008), 'nb_heure'],
       breaks = seq(-0.25, 200, by = 1),
       plot = FALSE
     )
     lines(x$mids, x$density, col = 3)
     x <- hist(
-      ech.init[ech.init$annee %in% c(2009:2013), 'nbHeures'],
+      ech.init[ech.init$annee %in% c(2009:2013), 'nb_heure'],
       breaks = seq(-0.25, 200, by = 1),
       plot = FALSE
     )
     lines(x$mids, x$density, col = 4)
     x <- hist(
-      ech.init[ech.init$annee %in% c(2014:2018), 'nbHeures'],
+      ech.init[ech.init$annee %in% c(2014:2018), 'nb_heure'],
       breaks = seq(-0.25, 200, by = 1),
       plot = FALSE
     )
     lines(x$mids, x$density, col = 5)
     x <- hist(
-      ech.init[ech.init$annee %in% c(2019:2023), 'nbHeures'],
+      ech.init[ech.init$annee %in% c(2019:2023), 'nb_heure'],
       breaks = seq(-0.25, 200, by = 1),
       plot = FALSE
     )
