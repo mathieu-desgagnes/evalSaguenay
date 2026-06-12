21a684beb3eeeb075426ad9d57f76d00512f0e29
Mathieu Desgagnés <mathieu.desgagnes@dfo-mpo.gc.ca>
2026-06-12 16:10:24 -0400

    complétion des captures

--- R/capture.R
+++ R/capture.R
@@ -13,6 +13,7 @@ capture <- function(
   ech,
   nb_pecheurs_moy_par_strate,
   nb_jours_par_strate,
+  nb_pecheurs_tot_par_strate,
   output_dir
 ) {
   ##
@@ -77,7 +78,7 @@ capture <- function(
         'nbOgac',
         'nbTurbot'
       )],
-      nbPecheursMoy.strate = nbPecheursTot.site,
+      nbPecheursMoy.strate = nb_pecheurs_tot_par_strate,
       langue = i.langue
     )
     dev.off()
@@ -88,32 +89,32 @@ capture <- function(
   ##
   ## captures total, brute par annee
   ##
-  nomPng <- 'captTot3'
-  for (i.langue in c('fr', 'en', 'bil')) {
-    png(
-      file = file.path(output_dir, i.langue, paste0(nomPng, '.png')),
-      height = 8,
-      width = 10,
-      units = 'in',
-      res = 300
-    )
-    temp <- captureTotale3(
-      capt.pecheur = ech[, c(
-        'anneeGestion',
-        'nomSite',
-        'nbSebastes',
-        'nbMorue',
-        'nbOgac',
-        'nbTurbot'
-      )],
-      nbPecheursMoy.strate = apply(nbPecheursTot.site, 2, sum, na.rm = TRUE),
-      langue = i.langue
-    )
-    dev.off()
-  }
-  captTot <- temp
-  write.csv2(temp, file = file.path(output_dir, 'csv', paste0(nomPng, '.csv')))
-  ##
+  # nomPng <- 'captTot3'
+  # for (i.langue in c('fr', 'en', 'bil')) {
+  #   png(
+  #     file = file.path(output_dir, i.langue, paste0(nomPng, '.png')),
+  #     height = 8,
+  #     width = 10,
+  #     units = 'in',
+  #     res = 300
+  #   )
+  #   temp <- captureTotale3(
+  #     capt.pecheur = ech[, c(
+  #       'anneeGestion',
+  #       'nomSite',
+  #       'nbSebastes',
+  #       'nbMorue',
+  #       'nbOgac',
+  #       'nbTurbot'
+  #     )],
+  #     nbPecheursMoy.strate = apply(nbPecheursTot.site, 2, sum, na.rm = TRUE),
+  #     langue = i.langue
+  #   )
+  #   dev.off()
+  # }
+  # captTot <- temp
+  # write.csv2(temp, file = file.path(output_dir, 'csv', paste0(nomPng, '.csv')))
+  # ##
 
   ##
   ## nbTot et proportion des espèces capturées, 1x2
@@ -128,8 +129,8 @@ capture <- function(
       res = 300
     )
     par(mfrow = c(1, 2))
-    temp <- captureTot(capt = captTot.site.sfs, langue = i.langue)
-    temp <- propEspeceCapt(
+    temp <- graph_capture_prop_esp(capt = captTot.site.sfs, langue = i.langue)
+    temp <- graph_capture_prop_esp2(
       capt = captTot.site.sfs,
       titre = 'B',
       langue = i.langue
